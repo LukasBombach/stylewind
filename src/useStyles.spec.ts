2c950fc5b20a432d01c64c853ecf8f5c215c4067
Lukas Bombach <lukas.bombach@googlemail.com>
2020-07-26 13:57:21 +0200

    refactored useStyles spec

--- src/useStyles.spec.ts
+++ src/useStyles.spec.ts
@@ -6,37 +6,34 @@ import type { Value } from "./parseCss";
 import type { TailwindClasses } from "./generated/classes";
 
 type Paramter = [typeof props[number]["name"], Value];
-
 type TailwindClass = TailwindClasses[number];
 
 function getAllPropParamters() {
-  const params: Paramter[] = [];
-  for (const { name, values } of props) {
-    for (const value of values) {
-      params.push([name, value]);
-    }
-  }
-  return params;
+  return props.flatMap(({ name, values }) =>
+    (values as Readonly<Value[]>).map((value) => [name, value] as Paramter)
+  );
 }
 
 describe("useStyles", () => {
-  const propParameters = getAllPropParamters();
+  const allParams = getAllPropParamters();
+  const classParams = allParams.filter(([, value]) => value !== false);
+  const undefParams = allParams.filter(([, value]) => value === false);
 
-  test.each(propParameters)(
+  test.each(classParams)(
     "tailwind contains the generated class for { %s: %s }",
     (name, value) => {
       const props = { [name]: value };
       const className = useStyles(props);
-      if (value === false) {
-        expect(className).toBe(undefined);
-      } else {
-        expect(className).not.toBe(undefined);
-        expect(generatedClasses.includes(className as TailwindClass)).toBe(
-          true
-        );
-      }
+      expect(className).not.toBe(undefined);
+      expect(generatedClasses.includes(className as TailwindClass)).toBe(true);
     }
   );
+
+  test.each(undefParams)("retuns undefined for { %s: %s }", (name, value) => {
+    const props = { [name]: value };
+    const className = useStyles(props);
+    expect(className).toBe(undefined);
+  });
 
   test("preserves the original className", () => {
     const className = "foo bar";
@@ -63,10 +60,6 @@ describe("useStyles", () => {
   });
 
   test("there is no tailwind class useStyles cannot generate", () => {
-    const generatableClasses = propParameters.filter(
-      ([, value]) => value !== false
-    );
-
-    expect(generatableClasses.length).toBe(generatedClasses.length);
+    expect(classParams.length).toBe(generatedClasses.length);
   });
 });
