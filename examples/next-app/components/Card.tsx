4ffcebf8f7f45387666340c7f5e481de5adae3f6
Lukas Bombach <lukas.bombach@googlemail.com>
2020-07-26 21:01:29 +0200

    dev setup works with examples

---
+++ examples/next-app/components/Card.tsx
@@ -0,0 +1,67 @@
+import styled from "stylewind";
+
+const Container = styled.article({
+  max: "w-sm",
+  overflow: "hidden",
+  shadow: "lg",
+  rounded: true,
+});
+
+const Img = styled("img", {
+  w: "full",
+});
+
+const Header = styled.header({
+  px: 6,
+  py: 4,
+});
+
+const Headline = styled.h2({
+  font: "bold",
+  text: "xl",
+  mb: 2,
+});
+
+const Intro = styled.p({
+  text: "gray-700", //["gray-700", "base"],
+});
+
+const Body = styled.p({
+  px: 6,
+  py: 4,
+});
+
+const Tag = styled.span({
+  inline: "block",
+  bg: "gray-200",
+  rounded: "full",
+  px: 3,
+  py: 1,
+  text: "sm", // ["sm", "gray-700"]
+  font: "semibold",
+  mr: 2,
+});
+
+const Card = () => (
+  <Container>
+    <Img
+      src="https://tailwindcss.com/img/card-top.jpg"
+      alt="Sunset in the mountains"
+    />
+    <Header>
+      <Headline>The Coldest Sunset</Headline>
+      <Intro>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </Intro>
+    </Header>
+    <Body>
+      <Tag>#photography</Tag>
+      <Tag>#travel</Tag>
+      <Tag>#winter</Tag>
+    </Body>
+  </Container>
+);
+
+export default Card;
---
+++ examples/next-app/pages/index.tsx
@@ -0,0 +1,22 @@
+import styled from "stylewind";
+import Card from "../components/Card";
+
+const Layout = styled.main({
+  container: true,
+  mx: "auto",
+});
+
+const Headline = styled.h1({
+  font: "bold",
+  text: "3xl",
+  mb: 2,
+});
+
+const IndexPage = () => (
+  <Layout>
+    <Headline>Hello Next.js 👋</Headline>
+    <Card />
+  </Layout>
+);
+
+export default IndexPage;
