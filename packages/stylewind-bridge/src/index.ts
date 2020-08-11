6811cb37ab4ea49aca88a156f79f8d52f0d27b6f
Lukas Bombach <lukas.bombach@googlemail.com>
2020-08-11 09:43:39 +0200

    code cleanup

--- packages/stylewind-bridge/src/index.ts
+++ packages/stylewind-bridge/src/index.ts
@@ -1,15 +1,3 @@
-import { getTailWindClassNames } from "./getTailWindCss";
-import splitClassName from "./splitClassName";
+import getProps from "./getProps";
 
-getTailWindClassNames().then(classNames => {
-  const values: Record<string, (string | number | true)[]> = {};
-
-  for (const className of classNames) {
-    const { name, value, prefix } = splitClassName(className);
-    values[name] = values[name] || [];
-    values[name].push(`${prefix}${value}`);
-    values[name].sort();
-  }
-
-  console.log(values);
-});
+getProps().then(props => console.log(props));
