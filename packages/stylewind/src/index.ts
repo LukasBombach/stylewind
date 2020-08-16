b414269cf2f3dca4dccc8703277b2866470a2992
Lukas Bombach <lukas.bombach@googlemail.com>
2020-08-16 14:40:40 +0200

    moved stuff, created build

---
+++ packages/stylewind/src/index.ts
@@ -0,0 +1,3 @@
+export { default } from "./styled";
+
+export type { StylewindProps } from "stylewind-bridge";
--- packages/stylewind/src/styled.ts
+++ packages/stylewind/src/styled.ts
@@ -3,16 +3,16 @@ import useStyledProps from "./useStyledProps";
 import { tags } from "./tags";
 
 import type { FC, ComponentProps } from "react";
-import type { Props } from "stylewind-bridge";
+import type { StylewindProps } from "stylewind-bridge";
 import type { Tag } from "./tags";
 
-export type StyledComponent<T extends Tag> = FC<ComponentProps<T> & Props>;
+export type StyledComponent<T extends Tag> = FC<ComponentProps<T> & StylewindProps>;
 
 export type TagMethods = {
-  [T in Tag]: (props: Props) => StyledComponent<T>;
+  [T in Tag]: (props: StylewindProps) => StyledComponent<T>;
 };
 
-function createComponent<T extends Tag>(el: T, props: Props): StyledComponent<T> {
+function createComponent<T extends Tag>(el: T, props: StylewindProps): StyledComponent<T> {
   return componentProps => {
     const mergedProps = Object.assign({}, props, componentProps);
     const styledProps = useStyledProps(mergedProps);
@@ -22,7 +22,7 @@ function createComponent<T extends Tag>(el: T, props: Props): StyledComponent<T>
 
 function getTagMethods(): TagMethods {
   const tagMethods = {} as TagMethods;
-  tags.forEach(tag => Object.assign(tagMethods, { [tag]: (props: Props) => createComponent(tag, props) }));
+  tags.forEach(tag => Object.assign(tagMethods, { [tag]: (props: StylewindProps) => createComponent(tag, props) }));
   return tagMethods;
 }
 
