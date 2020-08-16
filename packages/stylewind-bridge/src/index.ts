f29fc005d421ae715e58e62d2b41622be59fcbc2
Lukas Bombach <lukas.bombach@googlemail.com>
2020-08-16 14:05:55 +0200

    created type generator

--- packages/stylewind-bridge/src/index.ts
+++ packages/stylewind-bridge/src/index.ts
@@ -1,3 +1,40 @@
+// import util from "util";
+import path from "path";
+import { promises as fs } from "fs";
 import getProps from "./getProps";
 
-getProps().then(props => console.log(props));
+const dist = (p = "") => path.resolve(__dirname, "../dist", p);
+
+// function inspect(value: any) {
+//   console.log(util.inspect(value, { depth: null, colors: true, maxArrayLength: Infinity, compact: false }));
+// }
+
+async function getInterface() {
+  const props = await getProps();
+  const entries = Object.entries(props).sort(([a], [b]) => (a > b ? 1 : -1));
+  const types = entries.map(([name, values]) => {
+    const valuesType = values.map(v => JSON.stringify(v));
+    return [name.toUpperCase(), valuesType.join(" | ")];
+  });
+  return types
+    .map(([name, value]) => {
+      return `type ${name} = ${value};`;
+    })
+    .join(`\n`);
+}
+
+async function run() {
+  const contents = await getInterface();
+
+  await fs.rmdir(dist(), { recursive: true });
+  await fs.mkdir(dist());
+  await fs.writeFile(dist("props.ts"), contents);
+}
+
+run();
+
+// getProps().then(props => {
+//   const name = "text";
+//   const values = props[name].filter(v => !/:/.test(v.toString()));
+//   inspect(values);
+// });
