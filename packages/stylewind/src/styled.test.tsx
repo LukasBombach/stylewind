561ebbab903bc54085b79164decd31820ae4a2bc
Lukas Bombach <lukas.bombach@googlemail.com>
2020-08-16 15:52:29 +0200

    improved tests

--- packages/stylewind/src/styled.test.tsx
+++ packages/stylewind/src/styled.test.tsx
@@ -3,16 +3,21 @@ import { render } from "@testing-library/react";
 import styled from "./styled";
 import { tags } from "./tags";
 
-describe("styled", () => {
-  test.each(tags.map(t => [t, t]))('styled("%s") renders <%s />', tag => {
+describe.each(tags)("styled", tag => {
+  test(`styled("${tag}") renders <${tag} />`, () => {
     const Component = styled(tag);
     const { container } = render(<Component />);
     expect(container.firstElementChild).toMatchInlineSnapshot(`<${tag} />`);
   });
 
-  test.each(tags.map(t => [t, t]))("styled.%s() renders <%s />", tag => {
+  test(`styled.${tag}() renders <${tag} />`, () => {
     const Component = styled[tag]();
     const { container } = render(<Component />);
     expect(container.firstElementChild).toMatchInlineSnapshot(`<${tag} />`);
   });
+
+  /* test("prop", () => {
+    const Div = styled.div()
+
+  }) */
 });
