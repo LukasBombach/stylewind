9ce800480090935ff92b6bb54ed49d6c7416d8a2
Lukas Bombach <lukas.bombach@googlemail.com>
2022-01-03 21:26:23 +0100

    fixed types and stuff

--- packages/tailwind-react/src/index.ts
+++ packages/tailwind-react/src/index.ts
@@ -2,24 +2,22 @@ import { createElement } from "react";
 
 import type { FC } from "react";
 
-// function styled(className: string): FC;
-// function styled<P extends Record<string, unknown> = {}>(fn: (props: P) => string): FC<P>;
-// function styled<P extends Record<string, unknown> = {}>(param: (fn: (props: P) => string) | string ): FC<P> {}
-
 type Props = Record<string, unknown>;
 type TagName = keyof JSX.IntrinsicElements;
 type Styles = string | ((props: Props) => string);
 type ClassNameProp = { className?: string };
+type SFC<P extends Props = {}, S extends Styles = string> = FC<S extends string ? ClassNameProp : ClassNameProp & P>;
 
-function styled<P extends Props = {}, S = Styles>(
-  tagName: TagName,
-  styles: S
-): FC<S extends string ? ClassNameProp : ClassNameProp & P> {
-  return ({ children, ...props }) => {
+function styled<P extends Props = {}, S extends Styles = string>(tagName: TagName, styles: S): SFC<P, S> {
+  const StyledComponent: SFC<P, S> = ({ children, ...props }) => {
     const tailwindClasses = typeof styles === "function" ? styles(props) : styles;
     const className = props.className ? `${props.className} ${tailwindClasses}` : tailwindClasses;
     return createElement(tagName, { ...props, className }, children);
   };
+
+  StyledComponent.displayName = tagName;
+
+  return StyledComponent;
 }
 
 export default styled;
