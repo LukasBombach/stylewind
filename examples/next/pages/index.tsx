9382ecd2fe8773ba7fcb9facf96e3c9d28331fed
Lukas Bombach <lukas.bombach@googlemail.com>
2022-01-03 23:18:50 +0100

    nice multiline button

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -2,13 +2,32 @@ import type { NextPage } from "next";
 import styled from "tailwind-react";
 
 const Headline = styled("h1", "text-xl font-bold");
+
 const Link = styled("a", "text-m underline");
+
+const Button = styled(
+  "button",
+  `text-sm
+  font-semibold
+  bg-gray-800
+  text-gray-300
+  py-3
+  px-4
+  rounded-lg
+  hover:bg-gray-700
+  focus:outline-none
+  focus-visible:ring-2
+  focus-visible:ring-gray-700
+  focus-visible:ring-offset-2
+  focus-visible:ring-offset-gray-900`
+);
 
 const Home: NextPage = () => {
   return (
     <main>
       <Headline>Headline</Headline>
       <Link href="/">Link</Link>
+      <Button>Button</Button>
     </main>
   );
 };
